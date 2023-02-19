88b680fef1970bc8ad4536759a557372b3e7651d
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-20 01:11:33 +0400

    upd

--- assets_tracker_bot/Bot/.venv_example.bat
+++ assets_tracker_bot/Bot/.venv_example.bat
@@ -1,4 +1,5 @@
-set ARCH_TYPE=<VM or Cloud or Lite>
+set BOT_ARCH_TYPE=<VM or Cloud or Lite>
 set BOT_ADDRESS=@<your_tg_bot_name>
-set API_KEY=<your_api_key>
+set BOT_FSM_STORAGE_TYPE=<type_of_storage_where_interim_answers_are_kept>
+set BOT_API_TOKEN=<your_api_token>
 set USER_DB_CONN=sqlite:///path/to/db/<db_name>.db
