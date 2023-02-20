0dfabf4160f3d969bb65c396491ff679161c12e1
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-21 03:00:46 +0400

    added connector class

--- assets_tracker_bot/Bot/DBMSconnection.py
+++ assets_tracker_bot/Bot/DBMSconnection.py
@@ -1,1 +1,30 @@
-# empty :(
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
+class DBMSCreateConnection:
+    """Async DMBS session connection class"""
+
+    def __init__(self, connection_string):
+        self.connection_str = connection_string
+        self.session = None
+
+    def __enter__(self):
+        engine = create_engine(self.connection_str)
+        session = sessionmaker()
+        self.session = session(bind=engine)
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+    # async def __aenter__(self):
+    #     engine = create_async_engine(self.connection_str)
+    #     self.session = AsyncSession(bind=engine)
+    #
+    #     return self
+    #
+    # async def __aexit__(self, exc_type, exc_val, exc_tb):
+    #     self.session.close()
