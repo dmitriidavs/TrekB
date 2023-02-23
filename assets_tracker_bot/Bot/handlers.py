323e8a2be1cb9ba7988a137f30be13d68c04f4e8
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-24 00:48:08 +0400

    added new handlers

--- assets_tracker_bot/Bot/handlers.py
+++ assets_tracker_bot/Bot/handlers.py
@@ -2,15 +2,12 @@ import asyncio
 
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from includes.keyboards import *
+from includes.fsm_clients import *
 
 
-class FSMClientSignUp(StatesGroup):
-    if_sign_up = State()
-    acc_config = State()
-
+# TODO: move answer msgs to db
 
 async def cmd_start(message: types.Message) -> None:
     """/start command handler"""
@@ -22,11 +19,8 @@ async def cmd_start(message: types.Message) -> None:
           'My data-driven insights will help you make the best out of your investment portfolio!'
     await message.answer(text=msg)
     await asyncio.sleep(1)
-    msg = 'You can get to know me better by pressing /info. ' \
-          'Or you can just go ahead and /sign_up.'
-    await message.answer(text=msg)
-    await asyncio.sleep(1)
-    msg = 'It\'s free!'
+    msg = 'You can learn more about me by pressing /info. ' \
+          'Or you can just go ahead and /join'
     await message.answer(text=msg, reply_markup=kb_start)
 
 
@@ -40,36 +34,62 @@ async def cmd_info(message: types.Message) -> None:
     await message.answer(text=msg)
 
 
-async def cmd_sign_up(message: types.Message) -> None:
-    """/sign_up command handler"""
+async def cmd_join(message: types.Message) -> None:
+    """/join command handler"""
 
-    await FSMClientSignUp.if_sign_up.set()
-    msg = 'Are you sure you want to subscribe? (Y/N)'
+    # await FSMJoin.is_join.set()
+    msg = 'All right, let\'s set you up!'
+    await message.answer(text=msg)
+    await asyncio.sleep(1)
+    msg = 'There are 2 ways to start configuring your portfolio:\n' \
+          '/manual - add assets by hand\n' \
+          '/import - import crypto wallet balance'
+    await message.answer(text=msg, reply_markup=kb_join)
+
+
+async def cmd_manual(message: types.Message) -> None:
+    """/manual command handler: start FSMManualSetup"""
+
+    await FSMManualSetup.asset_name.set()    # start fsm
+    msg = 'OK. Send me the name of an asset.'
     await message.answer(text=msg)
 
 
-async def cmd_if_sign_up(message: types.Message, state: FSMContext) -> None:
-    """New user auth step"""
+async def add_asset_name(message: types.Message, state: FSMContext) -> None:
+    """
+    FSMManualSetup.asset_name:
+    Validating and saving name of an asset
+    """
 
-    if message.text == 'Y':
-        msg = 'All right! Let\'s sign you up.'
-        await message.answer(text=msg)
-        await state.finish()
-    elif message.text == 'N':
-        msg = 'Sorry to hear that!'
-        await message.answer(text=msg)
-        await state.finish()
-    else:
-        msg = 'Only Y/N answers accepted!'
+    async with state.proxy() as data:
+        data["asset_name"] = message.text
+    await FSMManualSetup.next()
+    msg = 'Now send me the quantity.'
+    await message.answer(text=msg)
+
+
+async def add_asset_quantity(message: types.Message, state: FSMContext) -> None:
+    """
+    FSMManualSetup.asset_quantity:
+    Validating and saving quantity of an asset
+    """
+
+    async with state.proxy() as data:
+        data["asset_quantity"] = float(message.text)
+
+    async with state.proxy() as data:
+        msg = f'Added {data["asset_quantity"]} {data["asset_name"]} to your portfolio.'
         await message.answer(text=msg)
 
-# TODO: add crypto wallet address for quick portfolio setup
+    await state.finish()
 
 
 def register_handlers(disp: Dispatcher) -> None:
-    """Handlers assembly"""
+    """External handler registration for easy imports"""
 
     disp.register_message_handler(cmd_start, commands=['start'])
     disp.register_message_handler(cmd_info, commands=['info'])
-    # disp.register_message_handler(cmd_sign_up, commands=['sign_up'], state=None)
-    # disp.register_message_handler(cmd_if_sign_up, commands=['cmd_if_sign_up'], state=FSMClientSignUp.if_sign_up)
+    disp.register_message_handler(cmd_join, commands=['join'])
+    disp.register_message_handler(cmd_manual, commands=['manual', 'add'], state=None)
+    disp.register_message_handler(add_asset_name, state=FSMManualSetup.asset_name)
+    disp.register_message_handler(add_asset_quantity, state=FSMManualSetup.asset_quantity)
