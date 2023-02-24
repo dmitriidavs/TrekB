de3354da41a320966645e50e29ad9ca98935b72a
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-24 21:47:06 +0400

    new kebabs

--- assets_tracker_bot/Bot/includes/keyboards.py
+++ assets_tracker_bot/Bot/includes/keyboards.py
@@ -1,4 +1,7 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
 
 start_1 = KeyboardButton('/info')
@@ -12,3 +15,9 @@ join_2 = KeyboardButton('/import')
 
 kb_join = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb_join.row(join_1, join_2)
+
+manual_1 = KeyboardButton('/add')
+manual_2 = KeyboardButton('/portfolio')
+
+kb_manual = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_manual.add(manual_1).add(manual_2)
