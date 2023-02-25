1aae1ed3890e11d770793454830f29e8f558f706
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-25 04:36:46 +0400

    logic

--- assets_tracker_bot/Bot/handlers.py
+++ assets_tracker_bot/Bot/handlers.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 
+from utils import *
 from includes.keyboards import *
 from includes.finite_state_machines import *
 
@@ -12,18 +13,23 @@ from includes.finite_state_machines import *
 async def cmd_start(message: types.Message) -> None:
     """/start command handler"""
 
-    # TODO: check if user is already in DB. If: | If not:
-
-    msg = 'Hey, I\'m TrekB! Glad you\'re here!'
-    await message.answer(text=msg)
-    await asyncio.sleep(1)
-    msg = 'I\'m your best friend when it comes to assets tracking. ' \
-          'My data-driven insights will help you make the best out of your investment portfolio!'
-    await message.answer(text=msg)
-    await asyncio.sleep(1)
-    msg = 'You can learn more about me by pressing /info. ' \
-          'Or you can just go ahead and /join.'
-    await message.answer(text=msg, reply_markup=kb_start)
+    # check if user has a portfolio already
+    if await user_is_active(message.from_user.id):
+        msg = 'Hey, you already have a portfolio!\n' \
+              'What would you like to do with it?'
+        await message.answer(text=msg, reply_markup=kb_start_active)
+    # if not
+    else:
+        msg = 'Hey, I\'m TrekB! Glad you\'re here!'
+        await message.answer(text=msg)
+        await asyncio.sleep(1)
+        msg = 'I\'m your best friend when it comes to assets tracking. ' \
+              'My data-driven insights will help you make the best out of your investment portfolio!'
+        await message.answer(text=msg)
+        await asyncio.sleep(1)
+        msg = 'You can learn more about me by pressing /info. ' \
+              'Or you can just go ahead and /join.'
+        await message.answer(text=msg, reply_markup=kb_start)
 
 
 async def cmd_info(message: types.Message) -> None:
@@ -86,12 +92,16 @@ async def add_asset_quantity(message: types.Message, state: FSMContext) -> None:
     await state.finish()
 
 
+# async def cmd_portfolio(message: types.Message) -> None:
+
+
 def register_handlers(disp: Dispatcher) -> None:
     """External handler registration for easy imports"""
 
     disp.register_message_handler(cmd_start, commands=['start'])
     disp.register_message_handler(cmd_info, commands=['info'])
     disp.register_message_handler(cmd_join, commands=['join'])
-    disp.register_message_handler(cmd_manual_add, commands=['manual', 'add'], state=None)
+    disp.register_message_handler(cmd_manual_add, commands=['add'], state=None)
     disp.register_message_handler(add_asset_name, state=FSMManualSetup.asset_name)
     disp.register_message_handler(add_asset_quantity, state=FSMManualSetup.asset_quantity)
+    # disp.register_message_handler(cmd_portfolio, commands=['portfolio'])
