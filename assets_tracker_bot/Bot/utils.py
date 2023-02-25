82e0a77d8cf5c3d4d4f3847a5299f813950a9e1d
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-25 04:37:06 +0400

    upd

--- assets_tracker_bot/Bot/utils.py
+++ assets_tracker_bot/Bot/utils.py
@@ -1,1 +1,10 @@
-# soon
+import datetime as dt
+
+from sqlite3 import Error as Sqlite3Error
+
+from includes.queries.user_db_queries import *
+from includes.DBMSconnection import DBMSCreateConnection
+
+
+async def user_is_active(user_id: str) -> bool:
+    pass
