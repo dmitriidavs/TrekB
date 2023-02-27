058e3fc5da3c0fc931f443d99ab7010395ceb413
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-27 23:24:45 +0400

    reworked db table logic

--- smart_portfolio_tracker/Bot/database/create_db.sql
+++ smart_portfolio_tracker/Bot/database/create_db.sql
@@ -4,17 +4,31 @@ CREATE TABLE IF NOT EXISTS assets(
     name varchar(30) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS users(
+-- TODO: convert timestamp to datetimetz in pg
+CREATE TABLE IF NOT EXISTS users (
     user_id integer PRIMARY KEY,
-    join_date datetime NOT NULL
+    has_portfolio boolean DEFAULT false,
+    join_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS portfolio(
+CREATE TABLE IF NOT EXISTS users_info (
+    user_id integer PRIMARY KEY,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NULL,
+    username varchar(30) NULL,
+    language_code varchar(2) NULL,
+    is_premium boolean NULL,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+-- move to SCD2
+CREATE TABLE IF NOT EXISTS portfolio (
     user_id integer NOT NULL,
     asset_id int2 NOT NULL,
     quantity float NOT NULL,
-    created_at datetime NOT NULL,
+    added_at timestamp DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(user_id, asset_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(asset_id) REFERENCES assets(asset_id)
 );
 
@@ -29,4 +43,4 @@ INSERT OR IGNORE INTO assets VALUES
     (6, 'GOOGL', 'Google'),
     (7, 'GOLD', 'Gold');
 
---PRAGMA foreign_keys = ON; as an additional command in Dockerfile
+-- PRAGMA foreign_keys = ON; as an additional command in Dockerfile
