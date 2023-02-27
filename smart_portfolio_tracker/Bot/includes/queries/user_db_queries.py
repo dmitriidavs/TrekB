56da3dcd07f25659f389b4fbb2c2126abd4edbc2
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-02-27 23:23:47 +0400

    added new user reg queries

--- smart_portfolio_tracker/Bot/includes/queries/user_db_queries.py
+++ smart_portfolio_tracker/Bot/includes/queries/user_db_queries.py
@@ -1,1 +1,25 @@
-SQL_DDL_USER_DB = """"""
+SQL_USER_HAS_PORTFOLIO = """
+SELECT EXISTS(
+    SELECT 1 FROM users
+    WHERE user_id = {user_id}
+);
+"""
+
+SQL_ADD_NEW_USER = """
+INSERT INTO users (
+    user_id
+)
+VALUES (
+    {user_id}
+);
+"""
+
+SQL_ADD_NEW_USER_INFO = """
+INSERT INTO users_info (
+    user_id, first_name, last_name,
+    username, language_code, is_premium
+) VALUES (
+    {user_id}, '{user_first_name}', '{user_last_name}',
+    '{user_username}', '{user_language_code}', '{user_is_premium}'
+);
+"""
