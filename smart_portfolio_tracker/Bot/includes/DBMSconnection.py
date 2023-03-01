4393705135bdf7b176fb9d1aaea3276170a8348e
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-01 20:55:04 +0400

    Update DBMSconnection.py

--- smart_portfolio_tracker/Bot/includes/DBMSconnection.py
+++ smart_portfolio_tracker/Bot/includes/DBMSconnection.py
@@ -1,24 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 
 class DBMSCreateConnection:
-    """Async [2B] DMBS session context manager"""
+    """Async DBMS session context manager"""
 
     def __init__(self, connection_string):
         self.connection_str = connection_string
         self.session = None
-
-    # def __enter__(self):
-    #     engine = create_engine(self.connection_str)
-    #     session = sessionmaker()
-    #     self.session = session(bind=engine)
-    #
-    #     return self
-    #
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     self.session.close()
 
     async def __aenter__(self):
         engine = create_async_engine(self.connection_str)
