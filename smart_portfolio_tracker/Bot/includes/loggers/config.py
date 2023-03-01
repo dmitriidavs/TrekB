0d036554c65b026d1a529f9facfc9a1bf1dc7e1a
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-02 00:29:23 +0400

    Create config.py

---
+++ smart_portfolio_tracker/Bot/includes/loggers/config.py
@@ -0,0 +1,8 @@
+import logging
+
+# TODO: move logging to online dashboard
+logging.basicConfig(format='[%(levelname)s]: %(message)s - %(asctime)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+basic_log = logging.getLogger(name='basic')
+basic_log.setLevel(logging.INFO)
