3fbe833d875d1e33fa7c3b676558ecd3ab850f49
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-01 20:20:56 +0400

    Update debug.py

--- smart_portfolio_tracker/Bot/includes/loggers/debug.py
+++ smart_portfolio_tracker/Bot/includes/loggers/debug.py
@@ -6,7 +6,7 @@ from creds import BOT_ARCH_TYPE, BOT_ADDRESS
 async def on_startup(_) -> None:
     """Log bot on start up phrase"""
 
-    print(f'[{BOT_ARCH_TYPE}] bot [{BOT_ADDRESS}] activated - {dt.datetime.now()}')
+    print(f'[{BOT_ARCH_TYPE}] bot [{BOT_ADDRESS}] is active - {dt.datetime.now()}')
 
 
 async def on_shutdown(_) -> None:
