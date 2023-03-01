b675dd89c83579a99cb884db62917f5dde59196a
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-02 00:29:32 +0400

    Update bot_debug.py

--- smart_portfolio_tracker/Bot/includes/loggers/bot_debug.py
+++ smart_portfolio_tracker/Bot/includes/loggers/bot_debug.py
@@ -1,15 +1,14 @@
-import logging
-
+from includes.loggers.config import basic_log
 from creds import BOT_ARCH_TYPE, BOT_ADDRESS
 
 
 async def on_startup(_) -> None:
     """Log bot on start up phrase"""
 
-    logging.info(f'{BOT_ARCH_TYPE} bot ({BOT_ADDRESS}) is active')
+    basic_log.info(f'{BOT_ARCH_TYPE} bot({BOT_ADDRESS}) is active')
 
 
 async def on_shutdown(_) -> None:
     """Log bot on shut down phrase"""
 
-    logging.info(f'{BOT_ARCH_TYPE} bot ({BOT_ADDRESS}) deactivated')
+    basic_log.info(f'{BOT_ARCH_TYPE} bot({BOT_ADDRESS}) deactivated')
