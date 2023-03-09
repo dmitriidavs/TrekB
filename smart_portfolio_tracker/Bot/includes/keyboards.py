efc622540d56227fc37454e44fdf8530920b19bf
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-09 05:47:20 +0400

    Update keyboards.py

--- smart_portfolio_tracker/Bot/includes/keyboards.py
+++ smart_portfolio_tracker/Bot/includes/keyboards.py
@@ -1,9 +1,4 @@
-from random import sample as random_sample
-
-from aiogram.types import (
-    ReplyKeyboardMarkup, KeyboardButton,
-    InlineKeyboardMarkup, InlineKeyboardButton
-)
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # TODO: optimize: one-time keyboard creation
 
@@ -14,7 +9,7 @@ start_active_2 = KeyboardButton('/flushit')
 
 kb_start = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_start.row(start_1, start_2)
-kb_start_active = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_start_active = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb_start_active.row(start_active_1, start_active_2)
 
 join_1 = KeyboardButton('/add')
@@ -28,18 +23,3 @@ manual_2 = KeyboardButton('/portfolio')
 
 kb_manual = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb_manual.add(manual_1).add(manual_2)
-
-
-flushit_clbcks = (
-    InlineKeyboardButton('No, back to portfolio', callback_data='flushit_no'),
-    InlineKeyboardButton('Nope, nevermind', callback_data='flushit_no'),
-    InlineKeyboardButton('Yes, delete my portfolio', callback_data='flushit_yes')
-)
-
-
-def get_flushit_kb() -> InlineKeyboardMarkup:
-    kb_flushit = InlineKeyboardMarkup()
-    for clbck in random_sample(flushit_clbcks, len(flushit_clbcks)):
-        kb_flushit.add(clbck)
-
-    return kb_flushit
