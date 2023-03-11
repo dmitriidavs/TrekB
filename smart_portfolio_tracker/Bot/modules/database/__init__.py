fffe91f92dc815e80a4f0565244ffcccc3d07fdb
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-12 01:03:53 +0400

    Update __init__.py

--- smart_portfolio_tracker/Bot/modules/database/__init__.py
+++ smart_portfolio_tracker/Bot/modules/database/__init__.py
@@ -1,1 +1,20 @@
-__all__ = ['DBMSconnection', 'queries_portfolio', 'queries_user', 'logic_portfolio', 'logic_user']
+__all__ = ['queries_portfolio', 'queries_user', 'logic_portfolio', 'logic_user',
+           'DBMSCreateConnection']
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
+class DBMSCreateConnection:
+    """Async DBMS session context manager"""
+
+    def __init__(self, connection_string):
+        self.connection_str = connection_string
+        self.session = None
+
+    async def __aenter__(self):
+        engine = create_async_engine(self.connection_str)
+        self.session = AsyncSession(bind=engine)
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.close()
