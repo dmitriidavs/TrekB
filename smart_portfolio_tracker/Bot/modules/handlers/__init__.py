7d16aeb4e4a1cce8cfa09de32df5143f7d5190b4
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-13 23:36:48 +0400

    Update __init__.py

--- smart_portfolio_tracker/Bot/modules/handlers/__init__.py
+++ smart_portfolio_tracker/Bot/modules/handlers/__init__.py
@@ -1,14 +1,15 @@
-__all__ = ['fsm', 'handlers_portfolio', 'handlers_user',
+__all__ = ['fsm', 'handlers_user', 'handlers_portfolio',
            'reg_hndlrs_user', 'reg_hndlrs_portfolio']
 
 from aiogram import Dispatcher
 
 from .handlers_user import *
 from .handlers_portfolio import *
+# from ..keyboards.callback import portfolio_cd
 
 
 def reg_hndlrs_user(disp: Dispatcher) -> None:
-    """User handler registration"""
+    """User handlers registration"""
 
     disp.register_message_handler(hndlr_start, commands=['start'])
     disp.register_message_handler(hndlr_info, commands=['info'])
@@ -16,7 +17,7 @@ def reg_hndlrs_user(disp: Dispatcher) -> None:
 
 
 def reg_hndlrs_portfolio(disp: Dispatcher) -> None:
-    """Portfolio handler registration"""
+    """Portfolio handlers & callbacks registration"""
 
     disp.register_message_handler(hndlr_portfolio, commands=['portfolio'])
     disp.register_message_handler(hndlr_flushit, commands=['flushit'])
@@ -24,3 +25,8 @@ def reg_hndlrs_portfolio(disp: Dispatcher) -> None:
     disp.register_message_handler(hndlr_manual_add, commands=['add'], state=None)
     disp.register_message_handler(stt_asset_name, state=FSMManualAdd.asset_name)
     disp.register_message_handler(stt_asset_quantity, state=FSMManualAdd.asset_quantity)
+
+    disp.register_callback_query_handler(cllbck_flushit_yes, text='flushit_yes')
+    disp.register_callback_query_handler(cllbck_flushit_back, text='flushit_no')
+    # disp.register_callback_query_handler(navigate, portfolio_cd.filter())
+
