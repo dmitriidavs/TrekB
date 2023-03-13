fe832833f3d6bebe74b9529e9f7a9fee99e6b585
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-13 23:36:53 +0400

    Update callback.py

--- smart_portfolio_tracker/Bot/modules/keyboards/callback.py
+++ smart_portfolio_tracker/Bot/modules/keyboards/callback.py
@@ -1,77 +1,88 @@
-# from aiogram.utils.callback_data import CallbackData
-#
-# from ..database.logic_portfolio import get_assets_outer, get_assets_inner
+from aiogram.utils.callback_data import CallbackData
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from ..database.logic_portfolio import get_assets_outer, get_assets_inner
 
 
-# portfolio_cd = CallbackData('get_portfolio', 'level', 'user_id', 'asset_id', 'added_at')
-# edit_asset = CallbackData('user_id', 'asset_id', 'added_at')
-#
-#
-# def create_callback_data(level: int, user_id: int, asset_id: int = 0, added_at: str = '0') -> str:
-#     return portfolio_cd.new(level=level, user_id=user_id,
-#                             asset_id=asset_id, added_at=added_at)
-#
-#
-# async def assets_outer_keyboard(user_id: int) -> InlineKeyboardMarkup:
-#     """Create user's assets inline keyboard"""
-#
-#     level = 0
-#     markup = InlineKeyboardMarkup()
-#
-#     assets_outer = await get_assets_outer(user_id=user_id)
-#     for asset in assets_outer:
-#         button_text = f'{asset[1]}: {asset[2]}'
-#         callback_data = create_callback_data(level=level+1,
-#                                              user_id=user_id)
-#         markup.add(
-#             InlineKeyboardButton(text=button_text, callback_data=callback_data)
-#         )
-#
-#     return markup
-#
-#
-# async def assets_inner_keyboard(user_id: int, asset_id: int) -> InlineKeyboardMarkup:
-#     """Create user's asset info"""
-#
-#     level = 1
-#     markup = InlineKeyboardMarkup()
-#
-#     assets_inner = await get_assets_inner(user_id=user_id, asset_id=asset_id)
-#     for asset_data in assets_inner:
-#         button_text = f'{asset_data[1]}: {asset_data[2]} - {asset_data[3]}'
-#         callback_data = create_callback_data(level=level + 1,
-#                                              user_id=user_id,
-#                                              asset_id=asset_data[0])
-#         markup.add(
-#             InlineKeyboardButton(text=button_text, callback_data=callback_data)
-#         )
-#     markup.row(
-#         InlineKeyboardButton(text='« back', callback_data=create_callback_data(level=level-1, user_id=user_id))
-#     )
-#
-#     return markup
-#
-#
-# async def edit_asset_inner_keyboard(user_id: int, asset_id: int, added_at: str) -> InlineKeyboardMarkup:
-#     """Edit user's asset info"""
+portfolio_cd = CallbackData('list_portfolio', 'level', 'user_id', 'asset_id', 'added_at')
+# edit_record_cd = CallbackData('edit_record', 'col', 'user_id', 'asset_id', 'added_at')
+# delete_record_cd = CallbackData('delete_record', 'user_id', 'asset_id', 'added_at')
+
+
+def create_cllbck_data(level: int, user_id: int, asset_id: int = 0, added_at: str = '0') -> str:
+    return portfolio_cd.new(level=level, user_id=user_id,
+                            asset_id=asset_id, added_at=added_at)
+
+
+async def assets_outer_keyboard(user_id: int) -> InlineKeyboardMarkup:
+    """Create user's portfolio inline keyboard"""
+
+    curr_level = 0
+    markup = InlineKeyboardMarkup()
+
+    assets_outer = await get_assets_outer(user_id=user_id)
+    # create a button for each asset in portfolio and add it to markup
+    for asset in assets_outer:
+        ticker_symbol = asset[0]
+        quantity_sum = asset[1]
+        button_text = f'{ticker_symbol}: {quantity_sum}'
+        cllbck_data = create_cllbck_data(level=curr_level+1, user_id=user_id)
+        markup.add(
+            InlineKeyboardButton(text=button_text, callback_data=cllbck_data)
+        )
+    # add navigation buttons
+
+    return markup
+
+
+async def assets_inner_keyboard(user_id: int, asset_id: int) -> InlineKeyboardMarkup:
+    """Create user's asset history inline keyboard"""
+
+    curr_level = 1
+    markup = InlineKeyboardMarkup()
+
+    assets_inner = await get_assets_inner(user_id=user_id, asset_id=asset_id)
+    # create a button for each asset's record in portfolio and add it to markup
+    for asset_data in assets_inner:
+        asset_id = asset_data[0]
+        quantity = asset_data[1]
+        added_dt = asset_data[2]
+        button_text = f'{quantity}: {added_dt}'
+        cllbck_data = create_cllbck_data(level=curr_level + 1,
+                                         user_id=user_id,
+                                         asset_id=asset_id)
+        markup.add(
+            InlineKeyboardButton(text=button_text, callback_data=cllbck_data)
+        )
+    # add navigation buttons
+    markup.row(
+        InlineKeyboardButton(text='« back', callback_data=create_cllbck_data(level=curr_level-1, user_id=user_id))
+    )
+
+    return markup
+
+
+# async def edit_asset_keyboard(user_id: int, asset_id: int, added_at: str) -> InlineKeyboardMarkup:
+#     """Edit user's asset history inline keyboard"""
 #
 #     level = 2
 #     markup = InlineKeyboardMarkup()
 #     markup.row(
-#         InlineKeyboardButton(text='Edit quantity', callback_data=update_asset_record(col='quantity',
-#                                                                                      user_id=user_id,
-#                                                                                      asset_id=asset_id,
-#                                                                                      added_at=added_at)),
-#         InlineKeyboardButton(text='Edit date', callback_data=update_asset_record(col='added_at',
-#                                                                                  user_id=user_id,
-#                                                                                  asset_id=asset_id,
-#                                                                                  added_at=added_at)),
-#         InlineKeyboardButton(text='Delete record', callback_data=delete_asset_record(user_id=user_id,
-#                                                                                      asset_id=asset_id,
-#                                                                                      added_at=added_at))
-#     ).add(
-#         InlineKeyboardButton(text='« back', callback_data=create_callback_data(level=level-1,
-#                                                                                user_id=user_id,
-#                                                                                asset_id=asset_id))
+#         InlineKeyboardButton(text='Edit quantity', callback_data=edit_record_cd.new(col='quantity',
+#                                                                                     user_id=user_id,
+#                                                                                     asset_id=asset_id,
+#                                                                                     added_at=added_at)),
+#         InlineKeyboardButton(text='Edit date', callback_data=edit_record_cd.new(col='added_at',
+#                                                                                 user_id=user_id,
+#                                                                                 asset_id=asset_id,
+#                                                                                 added_at=added_at)),
+#         InlineKeyboardButton(text='Delete record', callback_data=delete_record_cd.new(user_id=user_id,
+#                                                                                       asset_id=asset_id,
+#                                                                                       added_at=added_at))
+#     )
+#     markup.add(
+#         InlineKeyboardButton(text='« back', callback_data=create_cllbck_data(level=level-1,
+#                                                                              user_id=user_id,
+#                                                                              asset_id=asset_id))
 #     )
 #     return markup
