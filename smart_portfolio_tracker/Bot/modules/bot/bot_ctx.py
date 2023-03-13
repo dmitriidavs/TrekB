fea66efd4e572c6acd9b636aafc5fdc9a9287d58
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-13 23:36:34 +0400

    Update bot_ctx.py

--- smart_portfolio_tracker/Bot/modules/bot/bot_ctx.py
+++ smart_portfolio_tracker/Bot/modules/bot/bot_ctx.py
@@ -1,4 +1,4 @@
-from ..creds import BOT_ARCH_TYPE, BOT_ADDRESS
+from ..creds import BOT_ARCH_TYPE, BOT_ADDRESS, LOG_FOLDER_PATH
 
 
 async def on_startup(_) -> None:
@@ -8,7 +8,9 @@ async def on_startup(_) -> None:
           'TrekB | Smart Portfolio Tracker\n'
           ':copyright: (c) 2023 by Dmitrii Davletshin (@dmitriidavs)\n'
           ':license: Apache 2.0, see LICENSE for more details\n'
-          '---------------------------------------------------------')
+          '---------------------------------------------------------\n'
+          f'Logs are being written into {LOG_FOLDER_PATH} folder\n'
+          '---------------------------------------------------------\n')
     print(f'{BOT_ARCH_TYPE} bot({BOT_ADDRESS}) is active')
 
 
