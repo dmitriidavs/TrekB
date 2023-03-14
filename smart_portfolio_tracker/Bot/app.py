8c1b72525a7f6f8e4e01ded0f6afb987d7d4f4f5
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-15 01:53:46 +0400

    registering handlers no mo

--- smart_portfolio_tracker/Bot/app.py
+++ smart_portfolio_tracker/Bot/app.py
@@ -7,15 +7,10 @@ TrekB | Smart Portfolio Tracker
 from aiogram.utils import executor
 
 from modules.handlers import dp
-# from modules.handlers import reg_hndlrs_user, reg_hndlrs_portfolio
 from modules.bot.bot_ctx import on_startup, on_shutdown
 
 
 if __name__ == '__main__':
-    # # register modules
-    # reg_hndlrs_user(dp)
-    # reg_hndlrs_portfolio(dp)
-
     # TODO: move to webhook
     # start polling
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
