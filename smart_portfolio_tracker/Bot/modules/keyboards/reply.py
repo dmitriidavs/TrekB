468ccd11b7ee7e325049135beda85955aa943c45
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-16 02:20:59 +0400

    Update reply.py

--- smart_portfolio_tracker/Bot/modules/keyboards/reply.py
+++ smart_portfolio_tracker/Bot/modules/keyboards/reply.py
@@ -13,12 +13,10 @@ kb_start_active.row(start_active_1, start_active_2)
 
 join_1 = KeyboardButton('/add')
 join_2 = KeyboardButton('/import')
-
 kb_join = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb_join.row(join_1, join_2)
 
 manual_1 = KeyboardButton('/add')
 manual_2 = KeyboardButton('/portfolio')
-
 kb_manual = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb_manual.add(manual_1).add(manual_2)
