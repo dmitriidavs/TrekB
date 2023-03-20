426a47cbcd855812c346d64a6c5eb524cdbee12b
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-20 23:47:24 +0400

    moved placeholder to placeholder branch

--- smart_portfolio_tracker/Bot_placeholder/bot_create.py
+++
@@ -1,7 +0,0 @@
-from bot import ATCoreBot
-from creds import ARCH_TYPE, API_KEY
-
-
-BaseBot = ATCoreBot(ARCH_TYPE, API_KEY)
-bot = BaseBot.bot
-dispatcher = BaseBot.dispatcher
--- smart_portfolio_tracker/Bot_placeholder/creds.py
+++
@@ -1,5 +0,0 @@
-import os
-
-API_KEY = os.environ.get("API_KEY")
-ARCH_TYPE = os.environ.get("ARCH_TYPE")
-BOT_ADDRESS = os.environ.get("BOT_ADDRESS")
