b750f443a9fb9390462d0ee2d5ccc7dedda99d95
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-23 23:31:59 +0400

    ticker symbol validation and quan > 0

--- smart_portfolio_tracker/Bot/modules/validation/__init__.py
+++ smart_portfolio_tracker/Bot/modules/validation/__init__.py
@@ -1,7 +1,5 @@
-__all__ = ['validators', 'formatters',
-           'validate_env_vars', 'validate_ticker_symbol',
-           'validate_text_is_float', 'validate_date_format']
-
+__all__ = ['formatters', 'validators',
+           'validate_env_vars', 'validate_text_is_positive_float', 'validate_date_format']
 
 import datetime as dt
 
@@ -18,22 +16,12 @@ def validate_env_vars(env_vars: dict) -> dict:
         raise TypeError(err.json())
 
 
-async def validate_ticker_symbol(ticker: str) -> bool:
-    """Check if ticker symbol is valid"""
-
-    # TODO: add caching
-    if ticker == 'err':     # test
-        return False
-    else:
-        return True
-
-
-async def validate_text_is_float(text: str) -> bool:
+async def validate_text_is_positive_float(text: str) -> bool:
     """Check if text can be converted to float"""
 
     try:
-        float(text)
-        return True
+        if float(text) > 0:
+            return True
     except ValueError:
         return False
 
