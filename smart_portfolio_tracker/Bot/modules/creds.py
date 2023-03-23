14168b0f33553f217e467e8eaadfc10fb02d9a7f
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-03-23 23:31:01 +0400

    Update creds.py

--- smart_portfolio_tracker/Bot/modules/creds.py
+++ smart_portfolio_tracker/Bot/modules/creds.py
@@ -3,7 +3,8 @@ import os
 from .validation import validate_env_vars
 
 
-env_vars = {
+# validate environment variables
+env_vars = validate_env_vars({
     'bot_arch_type': os.environ.get('BOT_ARCH_TYPE'),
     'bot_address': os.environ.get('BOT_ADDRESS'),
     'bot_fsm_storage_type': os.environ.get('BOT_FSM_STORAGE_TYPE'),
@@ -14,10 +15,15 @@ env_vars = {
     'log_folder_path': os.environ.get('LOG_FOLDER_PATH'),
     'log_host': os.environ.get('LOG_HOST'),
     'log_port': os.environ.get('LOG_PORT'),
-}
+})
 
-# verify environment variables
-env_vars = validate_env_vars(env_vars)
-# create variables from env_vars keys in upper case
-for key, val in env_vars.items():
-    exec(key.upper() + '=val')
+BOT_ARCH_TYPE = env_vars['bot_arch_type']
+BOT_ADDRESS = env_vars['bot_address']
+BOT_FSM_STORAGE_TYPE = env_vars['bot_fsm_storage_type']
+BOT_API_TOKEN = env_vars['bot_api_token']
+USERS_DB_CONN = env_vars['users_db_conn']
+UTIL_DB_HOST = env_vars['util_db_host']
+UTIL_DB_PORT = env_vars['util_db_port']
+LOG_FOLDER_PATH = env_vars['log_folder_path']
+LOG_HOST = env_vars['log_host']
+LOG_PORT = env_vars['log_port']
