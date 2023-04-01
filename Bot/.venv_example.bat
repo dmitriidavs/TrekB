98dcf039ac4a2edc74794e41f309ec11853937f4
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-02 01:26:07 +0400

    Update .venv_example.bat

--- Bot/.venv_example.bat
+++ Bot/.venv_example.bat
@@ -1,12 +1,15 @@
-set BOT_ARCH_TYPE=<VM or Cloud or Lite>
+set BOT_ARCH_TYPE=Lite
 set BOT_ADDRESS=@<your_tg_bot_name>
-set BOT_FSM_STORAGE_TYPE=<memory or mongodb>
+set BOT_FSM_STORAGE_TYPE=<memory or redis>
 set BOT_API_TOKEN=<your_api_token>
 
 set USERS_DB_CONN=sqlite+aiosqlite:///path/to/db/<db_name>.db
 
-set UTIL_DB_HOST=localhost
-set UTIL_DB_PORT=6379
+set CACHE_HOST=localhost
+set CACHE_PORT=6379
+
+set BROKER_HOST=localhost
+set BROKER_PORT=6380
 
 set LOG_FOLDER_PATH=logs
 set LOG_HOST=
