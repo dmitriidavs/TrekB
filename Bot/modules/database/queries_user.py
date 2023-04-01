516367ddb679542218a993818dcf3ac2feb4019a
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-02 02:54:17 +0400

    injection safe ✔️

--- Bot/modules/database/queries_user.py
+++ Bot/modules/database/queries_user.py
@@ -1,7 +1,7 @@
 SQL_USER_EXISTS = """
 SELECT EXISTS (
     SELECT 1 FROM users
-    WHERE user_id = {user_id}
+    WHERE user_id = :user_id
 );
 """
 
@@ -9,7 +9,7 @@ SQL_USER_HAS_PORTFOLIO = """
 SELECT
     has_portfolio
 FROM users
-WHERE user_id = {user_id};
+WHERE user_id = :user_id;
 """
 
 SQL_ADD_NEW_USER = """
@@ -17,13 +17,13 @@ INSERT INTO users (
     user_id, first_name, last_name,
     username, language_code, is_premium
 ) VALUES (
-    {user_id}, '{user_first_name}', '{user_last_name}',
-    '{user_username}', '{user_language_code}', {user_is_premium}
+    :user_id, :user_first_name, :user_last_name,
+    :user_username, :user_language_code, :user_is_premium
 );
 """
 
 SQL_UPDATE_USER_HAS_PORTFOLIO_FLAG = """
 UPDATE users
-SET has_portfolio = {has_portfolio}
-WHERE user_id = {user_id};
+SET has_portfolio = :has_portfolio
+WHERE user_id = :user_id;
 """
