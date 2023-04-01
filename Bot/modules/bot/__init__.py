c21f435a57a7a5b30e5d3b6d48a4a4dbd6b762b0
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-02 02:11:07 +0400

    Update __init__.py

--- Bot/modules/bot/__init__.py
+++ Bot/modules/bot/__init__.py
@@ -2,13 +2,13 @@ __all__ = ['corebot', 'bot_ctx',
            'bot', 'dp']
 
 from .corebot import CoreBot
-from ..creds import BOT_API_TOKEN, BOT_FSM_STORAGE_TYPE, UTIL_DB_HOST, UTIL_DB_PORT
+from ..creds import BOT_API_TOKEN, BOT_FSM_STORAGE_TYPE, CACHE_HOST, CACHE_PORT
 
 
 Bot = CoreBot(
     api_token=BOT_API_TOKEN,
-    util_db_host=UTIL_DB_HOST,
-    util_db_port=UTIL_DB_PORT,
+    cache_host=CACHE_HOST,
+    cache_port=CACHE_PORT,
     storage_type=BOT_FSM_STORAGE_TYPE
 )
 bot = Bot.bot
