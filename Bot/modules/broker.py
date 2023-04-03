3c67ccbce0fdd7db969114c46e740cac8efa2177
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-03 23:20:17 +0400

    added decoding

--- Bot/modules/broker.py
+++ Bot/modules/broker.py
@@ -3,5 +3,6 @@ from aioredis import Redis
 from .creds import BROKER_HOST, BROKER_PORT
 
 
-cache = Redis(host=BROKER_HOST,
-              port=BROKER_PORT)
+broker = Redis(host=BROKER_HOST,
+               port=BROKER_PORT,
+               decode_responses=True)
