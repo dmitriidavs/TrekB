acb0953a4dc6a582040f67e04d15c6eded71192c
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-04 01:29:58 +0400

    added broker pointer generator

---
+++ Bot/modules/validation/utils.py
@@ -0,0 +1,12 @@
+import os
+
+import xxhash
+
+
+def gen_pointer(p_len: int = 8) -> str:
+    """Generate a random hash pointer for message broker to use as part of a key"""
+
+    rand_bytes = os.urandom(p_len)
+    hash_val = xxhash.xxh32(rand_bytes).intdigest()
+
+    return hex(hash_val)[2:p_len + 2]
