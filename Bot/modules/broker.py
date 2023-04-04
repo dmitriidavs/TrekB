5b5bc920fc88bbe65ea02e3fb40bdabb17eef27f
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-05 02:10:14 +0400

    implemented message broker, fixed: callback data len limitation & use of ":" error

--- Bot/modules/broker.py
+++ Bot/modules/broker.py
@@ -5,35 +5,43 @@ from aioredis import Redis
 from .validation.utils import gen_pointer
 
 
+# TODO: implement check for duplicate data (prefix?)
 class Broker(Redis):
     """
     Redis based message broker
     --------------------------
     set_data, get_data, del_data, multi_exec_data:
-        user has multiple choice (e.g.: callback buttons)
+        used when user has multiple choice (e.g.: callback buttons)
     else:
-        user in some FSM state
+        used when user in some FSM state
     """
 
-    async def set_data(self, user_id: int, data: Union[dict, tuple[tuple]]) -> Union[dict, tuple]:
-        """hset or multi/exec hset"""
+    async def set_data(self, user_id: int, data: Union[dict, tuple[tuple]]) -> Union[dict, tuple[dict]]:
+        """
+        data:
+            1. dict -> hset
+            2. tuple[tuple_of_data_tuples, tuple_of_str_attributes] -> multi/exec hset
+        """
 
         if isinstance(data, tuple):
-            data_dict = dict((data[i], data[i + 1]) for i in range(0, len(data), 2))
-            # data = [{}]
+            # start transaction
             async with self.pipeline(transaction=True) as pipe:
-                for b_data, b_name in zip(data[0], data[1]):
-                    pointer = gen_pointer()
-
-                    b_data["pointer"] = pointer
+                result = []
+                for b_data in data[0]:
+                    # create dict from data & attributes tuples
+                    res_dict = {key: val for key, val in zip(data[1], b_data)}
+                    pointer = await gen_pointer()
+                    res_dict["pointer"] = pointer
+                    result.append(res_dict)
+                    # add hset command to the pipe
                     await pipe.hset(name=f'{user_id}:{pointer}',
-                                    mapping=b_data)
-                    data.append(b_data)
+                                    mapping=res_dict)
                 # execute transaction
                 await pipe.execute()
-                return tuple(data)
+                # return resulting dictionaries
+                return tuple(result)
         else:
-            pointer = gen_pointer()
+            pointer = await gen_pointer()
             data["pointer"] = pointer
             await self.hset(name=f'{user_id}:{pointer}',
                             mapping=data)
