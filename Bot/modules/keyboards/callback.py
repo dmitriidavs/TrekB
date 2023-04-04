1bfd0f1b22a114e41aacac9bab17ff3244f59885
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-05 02:10:01 +0400

    implemented message broker, fixed: callback data len limitation & use of ":" error

--- Bot/modules/keyboards/callback.py
+++ Bot/modules/keyboards/callback.py
@@ -1,5 +1,4 @@
 from random import sample
-from types import MappingProxyType
 
 from aiogram.utils.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,12 +8,11 @@ from ..validation.formatters import format_float_to_currency, format_dt
 from .. import broker
 
 
-portfolio_cd = CallbackData('list_portfolio', 'level', 'sub_level', 'user_id', 'data')
+portfolio_cd = CallbackData('list_portfolio', 'level', 'sub_level', 'user_id', 'pointer')
 
 
-def create_cllbck_data(level: int, user_id: int, sub_level: int = -1,
-                       data: dict = MappingProxyType({'_data_err': -1})) -> str:
-    return portfolio_cd.new(level=level, sub_level=sub_level, user_id=user_id, data=data)
+def create_cllbck_data(level: int, user_id: int, pointer: str = '_pointer_err', sub_level: int = -1) -> str:
+    return portfolio_cd.new(level=level, sub_level=sub_level, user_id=user_id, pointer=pointer)
 
 
 async def assets_outer_keyboard(user_id: int) -> InlineKeyboardMarkup:
@@ -25,16 +23,20 @@ async def assets_outer_keyboard(user_id: int) -> InlineKeyboardMarkup:
 
     # request assets data from users DB
     assets_outer = await get_assets_outer(user_id=user_id)
+    attributes = ('asset_id', 'ticker_symbol', 'quantity_sum')
+
     # add broker message & get data w/pointers
-    assets_outer = broker.set_data(user_id=user_id,         # asset_id, ticker_symbol, quantity_sum
-                                   data=assets_outer)       # should be dict with pointer inside
+    new_broker_data = await broker.set_data(user_id=user_id, data=(assets_outer, attributes))
+
     # create a button for each asset in portfolio and add it to markup
-    for asset in assets_outer:
+    for b_data in new_broker_data:
         # create button text
-        quantity_sum_text = await format_float_to_currency(asset[2], 4)
-        button_text = f'{quantity_sum_text}   {asset[1]}'
+        quantity_sum_text = await format_float_to_currency(b_data["quantity_sum"], 4)
+        button_text = f'{quantity_sum_text}   {b_data["ticker_symbol"]}'
         # create callback data
-        cllbck_data = create_cllbck_data(level=curr_level+1, user_id=user_id, data=assets_outer)
+        cllbck_data = create_cllbck_data(level=curr_level+1,
+                                         user_id=user_id,
+                                         pointer=b_data["pointer"])
         # add button
         markup.insert(
             InlineKeyboardButton(text=button_text,
@@ -44,30 +46,30 @@ async def assets_outer_keyboard(user_id: int) -> InlineKeyboardMarkup:
     return markup
 
 
-async def assets_inner_keyboard(user_id: int, asset_id: int, ticker_symbol: str) -> InlineKeyboardMarkup:
+async def assets_inner_keyboard(user_id: int, broker_data: dict) -> InlineKeyboardMarkup:
     """Create user's asset history inline keyboard"""
 
     curr_level = 1
     sub_level = -1
     markup = InlineKeyboardMarkup()
 
-    assets_inner = await get_assets_inner(user_id=user_id, asset_id=asset_id)
+    # request asset's records data from users DB
+    assets_inner = await get_assets_inner(user_id=user_id, asset_id=broker_data["asset_id"])
+    attributes = ('asset_id', 'ticker_symbol', 'quantity', 'added_at')
+
+    # add broker message & get data w/pointers
+    new_broker_data = await broker.set_data(user_id=user_id, data=(assets_inner, attributes))
+
     # create a button for each asset's record in portfolio and add it to markup
-    for asset_data in assets_inner:
-        asset_id = asset_data[0]
-        quantity = asset_data[1]
-        added_at = asset_data[2]
+    for b_data in new_broker_data:
         # format quanity sum as currency & added_at as date
-        quantity_text = await format_float_to_currency(quantity, 4)
-        added_at_text = await format_dt(added_at)
+        quantity_text = await format_float_to_currency(b_data["quantity"], 4)
+        added_at_text = await format_dt(b_data["added_at"])
         # create callback data & add button
         button_text = f'+{quantity_text} | {added_at_text}'
         cllbck_data = create_cllbck_data(level=curr_level+1,
                                          user_id=user_id,
-                                         asset_id=asset_id,
-                                         ticker_symbol=ticker_symbol,
-                                         quantity=quantity,
-                                         added_at=added_at.replace(':', '+'))
+                                         pointer=b_data["pointer"])
         markup.add(
             InlineKeyboardButton(text=button_text,
                                  callback_data=cllbck_data)
@@ -79,16 +81,14 @@ async def assets_inner_keyboard(user_id: int, asset_id: int, ticker_symbol: str)
         InlineKeyboardButton(text='× delete',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               sub_level=sub_level+1,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol,
-                                                              user_id=user_id))
+                                                              user_id=user_id,
+                                                              pointer=broker_data["pointer"]))
     )
 
     return markup
 
 
-async def edit_asset_keyboard(user_id: int, asset_id: int, ticker_symbol: str,
-                              quantity: float, added_at: str) -> InlineKeyboardMarkup:
+async def edit_asset_keyboard(user_id: int, broker_data: dict) -> InlineKeyboardMarkup:
     """Edit user's asset history inline keyboard"""
 
     curr_level = 2
@@ -99,36 +99,26 @@ async def edit_asset_keyboard(user_id: int, asset_id: int, ticker_symbol: str,
                              callback_data=create_cllbck_data(level=curr_level,
                                                               sub_level=sub_level+1,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol,
-                                                              quantity=quantity,
-                                                              added_at=added_at)),
+                                                              pointer=broker_data["pointer"])),
         InlineKeyboardButton(text='Edit date',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               sub_level=sub_level+2,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol,
-                                                              quantity=quantity,
-                                                              added_at=added_at))
+                                                              pointer=broker_data["pointer"]))
     ).add(
         InlineKeyboardButton(text='× delete record', callback_data=create_cllbck_data(level=curr_level,
                                                                                       sub_level=sub_level+3,
                                                                                       user_id=user_id,
-                                                                                      asset_id=asset_id,
-                                                                                      ticker_symbol=ticker_symbol,
-                                                                                      quantity=quantity,
-                                                                                      added_at=added_at))
+                                                                                      pointer=broker_data["pointer"]))
     ).add(
         InlineKeyboardButton(text='« back', callback_data=create_cllbck_data(level=curr_level-1,
                                                                              user_id=user_id,
-                                                                             ticker_symbol=ticker_symbol,
-                                                                             asset_id=asset_id))
+                                                                             pointer=broker_data["pointer"]))
     )
     return markup
 
 
-async def delete_asset_history_keyboard(user_id: int, asset_id: int, ticker_symbol: str) -> InlineKeyboardMarkup:
+async def delete_asset_history_keyboard(user_id: int, broker_data: dict) -> InlineKeyboardMarkup:
     """Delete user's asset inline keyboard with buttons in random order"""
 
     curr_level = 1
@@ -139,14 +129,12 @@ async def delete_asset_history_keyboard(user_id: int, asset_id: int, ticker_symb
         InlineKeyboardButton(text='No, back to activity',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol)),
+                                                              pointer=broker_data["pointer"])),
         InlineKeyboardButton(text='Yes, delete asset data',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               sub_level=sub_level+1,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol)),
+                                                              pointer=broker_data["pointer"])),
         InlineKeyboardButton(text='Nope, back to portfolio',
                              callback_data=create_cllbck_data(level=curr_level-1,
                                                               user_id=user_id))
@@ -158,8 +146,7 @@ async def delete_asset_history_keyboard(user_id: int, asset_id: int, ticker_symb
     return markup
 
 
-async def delete_record_keyboard(user_id: int, asset_id: int, ticker_symbol: str,
-                                 quantity: float, added_at: str) -> InlineKeyboardMarkup:
+async def delete_record_keyboard(user_id: int, broker_data: dict) -> InlineKeyboardMarkup:
     """Delete user's asset record inline keyboard with buttons in random order"""
 
     curr_level = 2
@@ -170,18 +157,12 @@ async def delete_record_keyboard(user_id: int, asset_id: int, ticker_symbol: str
         InlineKeyboardButton(text='No, back to record',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol,
-                                                              quantity=quantity,
-                                                              added_at=added_at)),
+                                                              pointer=broker_data["pointer"])),
         InlineKeyboardButton(text='Yes, delete record data',
                              callback_data=create_cllbck_data(level=curr_level,
                                                               sub_level=sub_level+1,
                                                               user_id=user_id,
-                                                              asset_id=asset_id,
-                                                              ticker_symbol=ticker_symbol,
-                                                              quantity=quantity,
-                                                              added_at=added_at)),
+                                                              pointer=broker_data["pointer"])),
         InlineKeyboardButton(text='Nope, back to portfolio',
                              callback_data=create_cllbck_data(level=curr_level-2,
                                                               user_id=user_id))
