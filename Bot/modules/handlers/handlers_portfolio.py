b522fd722eada8b0fdd3b300ef9f2ccba7ec5812
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-05 02:10:05 +0400

    implemented message broker, fixed: callback data len limitation & use of ":" error

--- Bot/modules/handlers/handlers_portfolio.py
+++ Bot/modules/handlers/handlers_portfolio.py
@@ -70,7 +70,7 @@ async def list_portfolio(message: Union[Message, CallbackQuery], **kwargs) -> No
     """List user's portfolio"""
 
     # remove reply keyboard & inline keyboard with portfolio callbacks
-    markup = await assets_outer_keyboard(message.from_user.id)
+    markup = await assets_outer_keyboard(user_id=message.from_user.id)
     msg = f'{message.from_user.first_name}\'s Portfolio:'
     if isinstance(message, Message):
         await message.answer(text=msg, reply_markup=markup)
@@ -80,44 +80,42 @@ async def list_portfolio(message: Union[Message, CallbackQuery], **kwargs) -> No
 
 
 @log_ux(btn='/portfolio', clbck='asset_history')
-async def list_asset_history(callback: CallbackQuery, asset_id: int, ticker_symbol: str, **kwargs) -> None:
+async def list_asset_history(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset: list user's asset history"""
 
-    markup = await assets_inner_keyboard(callback.from_user.id, asset_id, ticker_symbol)
-    await callback.message.edit_text(text=f'{ticker_symbol} Activity:', reply_markup=markup)
+    markup = await assets_inner_keyboard(user_id=callback.from_user.id, broker_data=broker_data)
+    await callback.message.edit_text(text=f'{broker_data["ticker_symbol"]} Activity:', reply_markup=markup)
 
 
 @log_ux(btn='/portfolio', clbck='edit_asset')
-async def list_asset_editing(callback: CallbackQuery, asset_id: int, ticker_symbol: str,
-                             quantity: float, added_at: str, **kwargs) -> None:
+async def list_asset_editing(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> asset record: edit user's asset history"""
 
-    markup = await edit_asset_keyboard(callback.message.from_user.id, asset_id, ticker_symbol, quantity, added_at)
-    quantity_text = await format_float_to_currency(quantity, 6)
-    added_at_text = await format_dt(added_at.replace('+', ':'))
-    msg = f'Editing {ticker_symbol}:\n' \
+    markup = await edit_asset_keyboard(user_id=callback.from_user.id, broker_data=broker_data)
+    quantity_text = await format_float_to_currency(broker_data["quantity"], 6)
+    added_at_text = await format_dt(broker_data["added_at"])
+    msg = f'Editing {broker_data["ticker_symbol"]}:\n' \
           f'+{quantity_text} | {added_at_text}'
     await callback.message.edit_text(text=msg, reply_markup=markup)
 
 
 @log_ux(btn='/portfolio', clbck='edit_asset_quantity')
 @dp.message_handler(lambda message: 'Edit quantity' in message.text, state=None)
-async def edit_record_quantity(callback: CallbackQuery, asset_id: int, ticker_symbol: str,
-                               quantity: float, added_at: str, **kwargs) -> None:
+async def edit_record_quantity(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> edit quantity: updates user's asset quantity"""
 
     # start new state
     await FSMEditQuantity.new_asset_quantity.set()
 
-    msg = f'OK. Let\'s change {quantity} {ticker_symbol} added on ' \
-          f'{added_at.replace("+", ":")}.\nWhat\'s the new quantity?'
+    msg = f'OK. Let\'s change {broker_data["quantity"]} {broker_data["ticker_symbol"]} added on ' \
+          f'{broker_data["added_at"]}.\nWhat\'s the new quantity?'
     await callback.message.edit_text(text=msg)
 
     # set data for editing in broker
     await broker.set_asset_editing_data({
         'user_id': callback.from_user.id,
-        'asset_id': asset_id,
-        'added_at': added_at
+        'asset_id': broker_data["asset_id"],
+        'added_at': broker_data["added_at"]
     })
 
 
@@ -131,16 +129,16 @@ async def stt_edit_record_quantity(message: Message, state: FSMContext) -> None:
 
     if await validate_text_is_positive_float(message.text):
         # get editing data from cache
-        c_editing_data = await broker.get_asset_editing_data(message.from_user.id)
+        broker_editing_data = await broker.get_asset_editing_data(message.from_user.id)
         # delete cached editing data
         await broker.del_asset_editing_data(message.from_user.id)
 
         # update asset quanity in portfolio table
         await update_asset_record_data(col='quantity',
                                        val=message.text,
-                                       user_id=c_editing_data["user_id"],
-                                       asset_id=c_editing_data["asset_id"],
-                                       added_at=c_editing_data["added_at"].replace('+', ':'))
+                                       user_id=broker_editing_data["user_id"],
+                                       asset_id=broker_editing_data["asset_id"],
+                                       added_at=broker_editing_data["added_at"])
 
         # send OK reply message
         msg = f'Good! {message.text} is the new quantity.'
@@ -155,23 +153,22 @@ async def stt_edit_record_quantity(message: Message, state: FSMContext) -> None:
 
 @log_ux(btn='/portfolio', clbck='edit_asset_date')
 @dp.message_handler(lambda message: 'Edit date' in message.text, state=None)
-async def edit_record_date(callback: CallbackQuery, asset_id: int, ticker_symbol: str,
-                           quantity: float, added_at: str, **kwargs) -> None:
+async def edit_record_date(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> edit date: updates user's asset date"""
 
     # start new state
     await FSMEditDate.new_asset_date.set()
 
-    msg = f'OK. Let\'s change {quantity} {ticker_symbol} added on ' \
-          f'{added_at.replace("+", ":")}.\n' \
+    msg = f'OK. Let\'s change {broker_data["quantity"]} {broker_data["ticker_symbol"]} added on ' \
+          f'{broker_data["added_at"]}.\n' \
           f'Here is the format for the new date:\nYYYY-MM-DD hh:mm:ss'
     await callback.message.edit_text(text=msg)
 
     # set data for asset editing in broker
     await broker.set_asset_editing_data({
         'user_id': callback.from_user.id,
-        'asset_id': asset_id,
-        'added_at': added_at
+        'asset_id': broker_data["asset_id"],
+        'added_at': broker_data["added_at"]
     })
 
 
@@ -185,16 +182,16 @@ async def stt_edit_record_date(message: Message, state: FSMContext) -> None:
 
     if await validate_date_format(message.text):
         # get editing data from broker
-        c_editing_data = await broker.get_asset_editing_data(message.from_user.id)
+        broker_editing_data = await broker.get_asset_editing_data(message.from_user.id)
         # delete editing data from broker
         await broker.del_asset_editing_data(message.from_user.id)
 
         # update asset quanity in portfolio table
         await update_asset_record_data(col='added_at',
                                        val=message.text,
-                                       user_id=c_editing_data["user_id"],
-                                       asset_id=c_editing_data["asset_id"],
-                                       added_at=c_editing_data["added_at"].replace('+', ':'))
+                                       user_id=broker_editing_data["user_id"],
+                                       asset_id=broker_editing_data["asset_id"],
+                                       added_at=broker_editing_data["added_at"])
 
         # send OK reply message
         msg = f'Good! {message.text} is the new date.'
@@ -209,48 +206,47 @@ async def stt_edit_record_date(message: Message, state: FSMContext) -> None:
 
 @log_ux(btn='/portfolio', clbck='list_delete_asset')
 @dp.message_handler(lambda message: message.text == '× delete')
-async def list_delete_asset_history(callback: CallbackQuery, asset_id: int, ticker_symbol: str, **kwargs) -> None:
+async def list_delete_asset_history(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> delete data: ask if user wants to delete asset data"""
 
-    markup = await delete_asset_history_keyboard(callback.message.from_user.id, asset_id, ticker_symbol)
-    msg = f'You\'re about to delete all of the {ticker_symbol} history. Are you sure?'
+    markup = await delete_asset_history_keyboard(user_id=callback.from_user.id, broker_data=broker_data)
+    msg = f'You\'re about to delete all of the {broker_data["ticker_symbol"]} history. Are you sure?'
     await callback.message.edit_text(text=msg, reply_markup=markup)
 
 
 @log_ux(btn='/portfolio', clbck='delete_asset')
 @dp.message_handler(lambda message: message.text == 'Yes, delete asset data')
-async def delete_asset_history(callback: CallbackQuery, asset_id: int, ticker_symbol: str, **kwargs) -> None:
+async def delete_asset_history(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> delete data: yes"""
 
-    await delete_asset_from_portfolio(callback.from_user.id, asset_id)
-    msg = f'{ticker_symbol} deleted'
+    await delete_asset_from_portfolio(user_id=callback.from_user.id, asset_id=broker_data["asset_id"])
+    msg = f'{broker_data["ticker_symbol"]} deleted'
     await callback.answer(text=msg)
     await list_portfolio(callback)
 
 
 @log_ux(btn='/portfolio', clbck='list_delete_record')
 @dp.message_handler(lambda message: message.text == '× delete record')
-async def list_delete_record_from_history(callback: CallbackQuery, asset_id: int, ticker_symbol: str,
-                                          quantity: float, added_at: str, **kwargs) -> None:
+async def list_delete_record_from_history(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> record -> delete data: ask if user wants to delete record data"""
 
-    markup = await delete_record_keyboard(callback.message.from_user.id, asset_id,
-                                          ticker_symbol, quantity, added_at)
-    msg = f'You\'re about to delete {quantity} {ticker_symbol} ' \
-          f'added on {added_at.replace("+", ":")}. Are you sure?'
+    markup = await delete_record_keyboard(user_id=callback.from_user.id, broker_data=broker_data)
+    msg = f'You\'re about to delete {broker_data["quantity"]} {broker_data["ticker_symbol"]} ' \
+          f'added on {broker_data["added_at"]}. Are you sure?'
     await callback.message.edit_text(text=msg, reply_markup=markup)
 
 
 @log_ux(btn='/portfolio', clbck='delete_asset')
 @dp.message_handler(lambda message: message.text == 'Yes, delete record data')
-async def delete_record_from_history(callback: CallbackQuery, asset_id: int, ticker_symbol: str,
-                                     quantity: float, added_at: str, **kwargs) -> None:
+async def delete_record_from_history(callback: CallbackQuery, broker_data: dict) -> None:
     """/portfolio -> asset -> record -> delete data: yes"""
 
-    await delete_record_from_portfolio(callback.from_user.id, asset_id, added_at.replace('+', ':'))
-    msg = f'-{quantity} {ticker_symbol} | {added_at.replace("+", ":")}'
+    await delete_record_from_portfolio(user_id=callback.from_user.id,
+                                       asset_id=broker_data["asset_id"],
+                                       added_at=broker_data["added_at"])
+    msg = f'-{broker_data["quantity"]} {broker_data["ticker_symbol"]} | {broker_data["added_at"]}'
     await callback.answer(text=msg)
-    await list_asset_history(callback, asset_id, ticker_symbol)
+    await list_asset_history(callback, broker_data)
 
 
 # TODO: add caching
@@ -261,10 +257,9 @@ async def navigate(callback: CallbackQuery, callback_data: dict) -> None:
     curr_level = callback_data["level"]
     sub_level = callback_data["sub_level"]
     user_id = callback_data["user_id"]
-    asset_id = callback_data["asset_id"]
-    ticker_symbol = callback_data["ticker_symbol"]
-    quantity = callback_data["quantity"]
-    added_at = callback_data["added_at"]
+    pointer = callback_data["pointer"]
+
+    broker_data = await broker.get_data(user_id=user_id, pointer=pointer)
 
     levels = {
         '0': {
@@ -286,12 +281,7 @@ async def navigate(callback: CallbackQuery, callback_data: dict) -> None:
 
     # main func is activated if sub_level is not provided
     curr_level_function = levels[curr_level]['main'] if sub_level == '-1' else levels[curr_level][sub_level]
-    await curr_level_function(callback,
-                              user_id=user_id,
-                              asset_id=asset_id,
-                              ticker_symbol=ticker_symbol,
-                              quantity=quantity,
-                              added_at=added_at)
+    await curr_level_function(callback, broker_data=broker_data)
 
 
 @log_ux(btn='/flushit')
