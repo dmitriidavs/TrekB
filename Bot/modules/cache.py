f220cfab1911ec754e0077d390748e92d62add1a
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-04 04:29:12 +0400

    Update cache.py

--- Bot/modules/cache.py
+++ Bot/modules/cache.py
@@ -1,7 +1,6 @@
 from aioredis import Redis
 
-from .creds import CACHE_HOST, CACHE_PORT
 
-
-cache = Redis(host=CACHE_HOST,
-              port=CACHE_PORT)
+class Cache(Redis):
+    """Redis based message cache"""
+    pass
