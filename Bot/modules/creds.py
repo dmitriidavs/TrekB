4b4fedc16decf6b3e86da01d0700e4f3f87ac073
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-06 01:20:47 +0400

    Update creds.py

--- Bot/modules/creds.py
+++ Bot/modules/creds.py
@@ -5,6 +5,7 @@ from .validation import validate_env_vars
 
 # validate environment variables
 env_vars = validate_env_vars({
+    'timezone': os.environ.get('TIMEZONE'),
     'bot_arch_type': os.environ.get('BOT_ARCH_TYPE'),
     'bot_address': os.environ.get('BOT_ADDRESS'),
     'bot_fsm_storage_type': os.environ.get('BOT_FSM_STORAGE_TYPE'),
@@ -14,11 +15,15 @@ env_vars = validate_env_vars({
     'cache_port': os.environ.get('CACHE_PORT'),
     'broker_host': os.environ.get('BROKER_HOST'),
     'broker_port': os.environ.get('BROKER_PORT'),
+    'cache_ttl': os.environ.get('CACHE_TTL'),
+    'fsm_ttl': os.environ.get('FSM_TTL'),
+    'broker_ttl': os.environ.get('BROKER_TTL'),
     'log_folder_path': os.environ.get('LOG_FOLDER_PATH'),
     'log_host': os.environ.get('LOG_HOST'),
     'log_port': os.environ.get('LOG_PORT'),
 })
 
+TIMEZONE = env_vars['timezone']
 BOT_ARCH_TYPE = env_vars['bot_arch_type']
 BOT_ADDRESS = env_vars['bot_address']
 BOT_FSM_STORAGE_TYPE = env_vars['bot_fsm_storage_type']
@@ -28,6 +33,9 @@ CACHE_HOST = env_vars['cache_host']
 CACHE_PORT = env_vars['cache_port']
 BROKER_HOST = env_vars['broker_host']
 BROKER_PORT = env_vars['broker_port']
+CACHE_TTL = env_vars['cache_ttl']
+FSM_TTL = env_vars['fsm_ttl']
+broker_ttl = env_vars['broker_ttl']
 LOG_FOLDER_PATH = env_vars['log_folder_path']
 LOG_HOST = env_vars['log_host']
 LOG_PORT = env_vars['log_port']
