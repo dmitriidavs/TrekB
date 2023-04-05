81d6af44a5d731565fcfbf6cd9f46b72e2a4d0b6
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-05 22:26:06 +0400

    added hashing utils for broker

--- Bot/modules/validation/utils.py
+++ Bot/modules/validation/utils.py
@@ -1,12 +1,14 @@
-import os
-
 import xxhash
+import msgpack
 
 
-async def gen_pointer(p_len: int = 8) -> str:
-    """Generate a random hash pointer for message broker to use as part of a key"""
+class HashDict:
+    """Hashing utilities"""
 
-    rand_bytes = os.urandom(p_len)
-    hash_val = xxhash.xxh32(rand_bytes).intdigest()
+    @staticmethod
+    async def get_dict_hash(data: dict) -> str:
+        return xxhash.xxh64(msgpack.dumps(data, use_bin_type=True)).hexdigest()
 
-    return hex(hash_val)[2:p_len + 2]
+    @staticmethod
+    async def compare_hashes(hash1: str, hash2: str) -> bool:
+        return hash1 == hash2
