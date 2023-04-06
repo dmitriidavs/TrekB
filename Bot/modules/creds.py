557b6de9fb0427386de3fdc20a3eb38a284b78bf
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-07 01:11:39 +0400

    Update creds.py

--- Bot/modules/creds.py
+++ Bot/modules/creds.py
@@ -18,24 +18,38 @@ env_vars = validate_env_vars({
     'cache_ttl': os.environ.get('CACHE_TTL'),
     'fsm_ttl': os.environ.get('FSM_TTL'),
     'broker_ttl': os.environ.get('BROKER_TTL'),
+    'log_type': os.environ.get('LOG_TYPE'),
     'log_folder_path': os.environ.get('LOG_FOLDER_PATH'),
     'log_host': os.environ.get('LOG_HOST'),
     'log_port': os.environ.get('LOG_PORT'),
+    'project_link': os.environ.get('PROJECT_LINK'),
+    'supported_assets_link': os.environ.get('SUPPORTED_ASSETS_LINK'),
 })
 
+# set environment variables
 TIMEZONE = env_vars['timezone']
+
 BOT_ARCH_TYPE = env_vars['bot_arch_type']
 BOT_ADDRESS = env_vars['bot_address']
 BOT_FSM_STORAGE_TYPE = env_vars['bot_fsm_storage_type']
 BOT_API_TOKEN = env_vars['bot_api_token']
+
 USERS_DB_CONN = env_vars['users_db_conn']
+
 CACHE_HOST = env_vars['cache_host']
 CACHE_PORT = env_vars['cache_port']
+
 BROKER_HOST = env_vars['broker_host']
 BROKER_PORT = env_vars['broker_port']
+
 CACHE_TTL = env_vars['cache_ttl']
 FSM_TTL = env_vars['fsm_ttl']
 BROKER_TTL = env_vars['broker_ttl']
+
+LOG_TYPE = env_vars['log_type']
 LOG_FOLDER_PATH = env_vars['log_folder_path']
 LOG_HOST = env_vars['log_host']
 LOG_PORT = env_vars['log_port']
+
+PROJECT_LINK = env_vars['project_link']
+SUPPORTED_ASSETS_LINK = env_vars['supported_assets_link']
