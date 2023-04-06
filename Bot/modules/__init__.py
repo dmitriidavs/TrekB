3b657e228ea27cba1a0aec4771f1adead0ab70fa
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-07 01:10:43 +0400

    Update __init__.py

--- Bot/modules/__init__.py
+++ Bot/modules/__init__.py
@@ -1,8 +1,8 @@
 __all__ = ['broker', 'cache', 'creds']
 
 from .creds import (
-    CACHE_HOST, CACHE_PORT,
-    BROKER_HOST, BROKER_PORT
+    CACHE_HOST, CACHE_PORT, CACHE_TTL,
+    BROKER_HOST, BROKER_PORT, BROKER_TTL
 )
 from .broker import Broker
 from .cache import Cache
@@ -10,7 +10,9 @@ from .cache import Cache
 
 broker = Broker(host=BROKER_HOST,
                 port=BROKER_PORT,
-                decode_responses=True)
+                decode_responses=True,
+                broker_ttl=BROKER_TTL)
 
 cache = Cache(host=CACHE_HOST,
-              port=CACHE_PORT)
+              port=CACHE_PORT,
+              cache_ttl=CACHE_TTL)
