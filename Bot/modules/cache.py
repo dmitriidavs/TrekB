62264187bd9c0e411afa6b1cc977f145b8452114
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-06 22:35:51 +0400

    added cache methods

--- Bot/modules/cache.py
+++ Bot/modules/cache.py
@@ -1,6 +1,17 @@
+from typing import Union
+
 from aioredis import Redis
+
+from .creds import CACHE_TTL
 
 
 class Cache(Redis):
     """Redis based cache"""
-    pass
+
+    async def set_data(self, key: str, value: Union[int, float, str]) -> None:
+        """set"""
+        await self.set(name=key, value=value, ex=CACHE_TTL)
+
+    async def get_data(self, key: str) -> Union[int, float, str]:
+        """get"""
+        return await self.get(name=key)
