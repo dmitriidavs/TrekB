877a998bb176795f83ca8d2724890abcff266319
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-14 23:57:09 +0400

    Update garbage_handler_dag.py

--- Airflow/dags/garbage_handler_dag.py
+++ Airflow/dags/garbage_handler_dag.py
@@ -9,7 +9,7 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.exceptions import AirflowException
 
-from creds import *
+from includes.creds import *
 
 
 args = {
@@ -31,7 +31,7 @@ def delete_dag(dag_name: str, dag_file_path: str) -> bool:
     """Delete dag"""
 
     # send delete request to Airflow API
-    url = f'http://{AIRFLOW_API_URL}/api/v1/dags/{dag_name}'
+    url = f'{AIRFLOW_API_URL}/{dag_name}'
     response = requests.delete(url,
                                auth=AIRFLOW_API_AUTH,
                                headers=AIRFLOW_API_HEADERS)
@@ -54,11 +54,24 @@ def delete_dag(dag_name: str, dag_file_path: str) -> bool:
 def delete_garbage_dags(dags_to_delete: tuple[tuple[str, str]], dags_folder_path: str) -> None:
     """Delete dags cloned from the original dags by users"""
 
-    for dag_name, dag_file_path in dags_to_delete:
-        # check that dag finished execution
+    for dag_name, dag_file_name in dags_to_delete:
+        # check that dag finished execution - TODO: dag_name as wildcard (dag_name + user_id)
+        last_dag_runs_url = f'{AIRFLOW_API_URL}/{dag_name}/dagRuns?order_by=-execution_date&state=success'
+        response = requests.get(last_dag_runs_url,
+                                auth=AIRFLOW_API_AUTH,
+                                headers=AIRFLOW_API_HEADERS)
+        if response.status_code != 200:
+            raise AirflowException(f'Could not retrieve DAGs: {response.text}')
 
-        # delete dag
-        delete_dag(dag_name, os.path.join(dags_folder_path, dag_file_path))
+        last_dag_runs = response.json()
+
+        print(last_dag_runs)
+
+        # # get dag file path
+        # dag_file_path = os.path.join(dags_folder_path, dag_file_name)
+        #
+        # # delete dag
+        # delete_dag(dag_name, dag_file_path)
 
 
 with DAG(dag_id='garbage_handler_dag',
@@ -70,10 +83,10 @@ with DAG(dag_id='garbage_handler_dag',
     )
     t2 = PythonOperator(
         task_id=f'delete_garbage_dags',
-        python_callable=on_api_trigger,
+        python_callable=delete_garbage_dags,
         op_kwargs={
             'dags_to_delete': DAGS_TO_DELETE,
-            'dags_folder': DAGS_FOLDER
+            'dags_folder': DAGS_FOLDER_PATH
         }
     )
     t3 = EmptyOperator(
