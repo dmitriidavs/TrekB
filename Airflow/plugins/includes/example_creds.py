459c4a37f658db24afba8899a1af99a9aa535f05
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-15 04:13:22 +0400

    Create example_creds.py

---
+++ Airflow/plugins/includes/example_creds.py
@@ -0,0 +1,13 @@
+AIRFLOW_API_URL = 'host:port'
+
+AIRFLOW_API_AUTH = ('usr', 'pwd')
+
+AIRFLOW_API_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+GARBAGE_DAG_PREFIXES = (
+    'some_name_1_dag',
+    'some_name_2_dag',
+)
