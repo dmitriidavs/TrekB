fdbfa504ba91888a1337fcc56027a4b6cca0be03
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-26 23:28:49 +0400

    added dag copy func

--- AWS/lambda/webhook/modules/utils.py
+++ AWS/lambda/webhook/modules/utils.py
@@ -1,11 +1,24 @@
+from typing import ClassVar
+
 import boto3
 
 from .creds import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
-def get_s3_client() -> None:
+def get_s3_client() -> ClassVar:
     """Connect to s3 & return client"""
 
     session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     return session.client('s3')
+
+
+def create_dag_copy(source_bucket: str, source_key: str,
+                    destination_bucket: str, destination_key: str) -> None:
+    """Create a copy of a base dag"""
+
+    get_s3_client().copy_object(
+        CopySource={'Bucket': source_bucket, 'Key': source_key},
+        Bucket=destination_bucket,
+        Key=destination_key
+    )
