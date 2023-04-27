7cae06a36af9f59880c2783f16eff95854193f96
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-27 23:10:56 +0400

    Update handlers_dags.py

--- AWS/lambda/webhook/modules/handlers/handlers_dags.py
+++ AWS/lambda/webhook/modules/handlers/handlers_dags.py
@@ -1,13 +1,28 @@
+import requests
 from aiogram.types import Message
+from aiogram.dispatcher import FSMContext
 
+from .fsm import FSMImportWallet
 from ..bot import dp
+from ..keyboards.callback import bc_networks_keyboard
+from ..utils import create_dag_copy
 
 
-@dp.message_handler(commands=['import'])
-async def hndlr_start(message: Message) -> None:
-    """/import command handler for importing wallet balance"""
+# @dp.message_handler(commands=['import'], state=None)
+# async def hndlr_import(message: Message) -> None:
+#     """/import command handler for importing wallet balance: start FSMImportWallet"""
+#
+#     await FSMImportWallet.blockchain.set()
+#     msg = 'OK. Firstly, choose one of the supported networks.'
+#     await message.answer(text=msg, reply_markup=bc_networks_keyboard())
+#
+#
+# @dp.message_handler(state=FSMImportWallet.blockchain)
+# async def stt_blockchain(message: Message, state: FSMContext) -> None:
+#     """
+#     FSMImportWallet.blockchain:
+#     """
 
-    msg = 'Wallet balance import is not supported in Lite version :('
-    await message.answer(text=msg)
-    
-    # gen DAG with custom params
+# make a copy of a base dag with user_id ()
+
+# activate DAG with custom params (requests)
