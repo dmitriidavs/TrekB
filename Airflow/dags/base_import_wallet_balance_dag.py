2d71cb5f815d432aa7de06fbc39a40ea4587ae92
dmitriidavs <dmitriidavs@gmail.com>
2023-05-03 23:42:39 +0400

    started import wallets dag

---
+++ Airflow/dags/base_import_wallet_balance_dag.py
@@ -0,0 +1,57 @@
+import datetime as dt
+
+from airflow.models import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.telegram_operator import TelegramOperator
+from moralis import evm_api
+
+from includes.CustomConnectors.DBMSconnection import DBMSCreateConnection
+from includes.CustomConnectors.context import ContextOperator
+from includes.utils.log import logger
+from includes.creds import *
+
+
+args = {
+    'owner': 'dmitriidavs',
+    'start_date': dt.datetime(2023, 5, 3, 10, 0, 0, 0),
+    'retries': 2,
+    'retry_delay': dt.timedelta(minutes=3),
+    'depends_on_past': False,
+}
+
+
+def get_wallet_balance(address: str, **context) -> None:
+    pass
+
+
+def apply_changes_in_portfolio() -> None:
+    pass
+
+
+with DAG(dag_id='import_wallet_balance_dag',
+         default_args=args,
+         schedule_interval='@once',
+         catchup=False) as dag:
+
+    t_start = EmptyOperator(
+        task_id='start_task'
+    )
+    t_get_wallet_balance = PythonOperator(
+        task_id=f'get_wallet_balance',
+        python_callable=get_wallet_balance,
+        op_kwargs={
+            'address': '',
+        }
+    )
+    t_send_ok_message = TelegramOperator(
+        task_id="send_ok_message_telegram",
+        telegram_conn_id=BOT_API_TOKEN,
+        chat_id=CHAT_ID,
+        text="Hello from Airflow!"
+    )
+    t_end = EmptyOperator(
+        task_id='end_task'
+    )
+
+    t_start >> t_get_wallet_balance >> t_send_ok_message >> t_end
