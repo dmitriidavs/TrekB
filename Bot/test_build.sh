7a4a87c677ee05f1d914bb7e547d0f8332f7dbcf
dmitriidavs <dmitriidavs@gmail.com>
2023-05-07 21:40:13 +0400

    nice

---
+++ Bot/test_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO before running: chmod +x test_build.sh
+
+IMAGE_NAME=$(sudo docker images --format '{{.Repository}}:{{.Tag}}' --no-trunc | sort -r | awk 'NR==1{print $1}')
+CNTNR_NAME=$(sudo docker ps -ql --format "{{.Names}}")
+
+sudo docker compose down
+sudo docker rmi ${IMAGE_NAME}
+sudo docker compose up -d
+sudo docker logs -f ${CNTNR_NAME}
