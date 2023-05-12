7ed6b918a122ae8abadbf3548f1f70702ea35634
dmitriidavs <dmitriidavs@gmail.com>
2023-05-12 22:25:49 +0400

    cleaning ubuntu is fun 👍

--- Bot/test_build.sh
+++ Bot/test_build.sh
@@ -5,6 +5,7 @@
 IMAGE_NAME=$(sudo docker images --format '{{.Repository}}:{{.Tag}}' --no-trunc | sort -r | awk 'NR==1{print $1}')
 CNTNR_NAME=$(sudo docker ps -ql --format "{{.Names}}")
 
+sudo docker system prune -af
 sudo docker compose down
 sudo docker rmi ${IMAGE_NAME}
 sudo docker compose up -d
