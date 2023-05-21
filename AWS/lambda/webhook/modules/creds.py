710444ebc28d0c36ce0b628facfb19d183a222b3
dmitriidavs <dmitriidavs@gmail.com>
2023-05-21 23:39:56 +0400

    Update creds.py

--- AWS/lambda/webhook/modules/creds.py
+++ AWS/lambda/webhook/modules/creds.py
@@ -8,3 +8,5 @@ WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+BUCKET_DAGS = os.environ.get('BUCKET_DAGS')
